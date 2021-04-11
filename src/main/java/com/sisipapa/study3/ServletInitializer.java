bac71caa41d53d759f7752c546083c0c743de520
KKH <coolguy239@naver.com>
2021-04-11 10:06:47 +0900

    Initial commit

---
+++ src/main/java/com/sisipapa/study3/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sisipapa.study3;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Study3Application.class);
+    }
+
+}
---
+++ src/test/java/com/sisipapa/study3/Study3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisipapa.study3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Study3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
